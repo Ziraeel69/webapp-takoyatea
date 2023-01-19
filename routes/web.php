f312e64169b6e4cc1ecfc4ffce9c313f2c712f11
ShylyHaste <rodrigo.raga110395@gmail.com>
2023-01-19 10:20:11 +0800

    gwapo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Route::get('/', function () {
+//     return view('homepage');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
