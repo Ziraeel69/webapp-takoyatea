3a47f1b0e82ea4d773190c1cfca59ddac01159b5
Ziraeel69 <johngumapac04@gmail.com>
2023-01-26 20:16:01 +0800

    css login button changes

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div>
-      <a class="btn-book-a-table" href="/login">Login</a>
+      <a class="btn-book-a-table" href="/login">LOGIN</a>
       <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
       <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
     </div>
