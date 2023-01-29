9b1fa8e205cf757b34e32284c5e589c90e584a65
Ziraeel69 <johngumapac04@gmail.com>
2023-01-29 20:45:07 +0800

    removing of un-useful things in dashboard and other changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
   <!-- Vendor CSS Files -->
   <link href="{{ asset ('assets-homepage/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   <link href="{{ asset ('assets-homepage/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset ('assets-homepage/assets/vendor/aos/aos.css" rel="stylesheet') }}">
+  <link href="{{ asset ('assets-homepage/assets/vendor/aos/aos.css') }}" rel="stylesheet">
   <link href="{{ asset ('assets-homepage/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
   <link href="{{ asset ('assets-homepage/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
